92a6197b52eda1e10bd18b63cede08ff323511ad
Pedro Trinkenreich <trinkpedro@gmail.com>
2025-07-02 14:18:17 -0300

    Correções menores

--- api/src/main/java/com/carlosribeiro/apirestfulv1/model/Usuario.java
+++ api/src/main/java/com/carlosribeiro/apirestfulv1/model/Usuario.java
@@ -1,7 +1,6 @@
 package com.carlosribeiro.apirestfulv1.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
